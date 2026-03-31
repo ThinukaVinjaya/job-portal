70895a5d3504026dbdf0fca3f9bbc5216a8df895
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2026-03-31 22:07:36 +0530

    update findjob page

--- src/FindJobs/Jobs.tsx
+++ src/FindJobs/Jobs.tsx
@@ -4,21 +4,20 @@ import JobCard from './JobCard'
 import { jobList } from '../Data/JobsData'
 
 const Jobs = () => {
-  return (
-    <div className='p-5'>
+    return (
+        <div className='p-5'>
 
-      <div className='flex justify-between' >
-        <div className='text-2xl font-semibold'>Recomended Jobs</div>
-            <Sort />      
-      </div>
-      <div className='mt-10 flex flex-wrap gap-5'>
-      {
-        jobList.map((job, index) => <JobCard key={index} {...job} /> )
-      }
-      </div>
-      <JobCard />
-    </div>
-  )
+            <div className='flex justify-between' >
+                <div className='text-2xl font-semibold'>Recomended Jobs</div>
+                <Sort />
+            </div>
+            <div className='mt-10 flex flex-wrap gap-11 justify-center'>
+                {
+                    jobList.map((job, index) => <JobCard key={index} {...job} />)
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Jobs
--- src/Pages/FingJobs.tsx
+++ src/Pages/FingJobs.tsx
@@ -6,7 +6,7 @@ import Jobs from '../FindJobs/Jobs';
 
 const FingJobs = () => {
     return (
-        <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
+        <div className="min-h-[80vh] bg-mine-shaft-950 font-['poppins']">
             <Divider  mr="xs" mx="md" />
             <SearchBar />
             <Divider  mr="xs" mx="md" />
