27508fc04ae321023372c5c74aa62d1bfc62a819
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2026-04-03 01:20:58 +0530

    update allign ment talentprofile page

--- src/Pages/TalentProfilePage.tsx
+++ src/Pages/TalentProfilePage.tsx
@@ -11,10 +11,10 @@ const TalentProfilePage = () => {
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] p-4">
             <Divider size="xs"  />
-            <Link className='my-4 ml-4 inline-block ' to="/find-talent">
+            <Link className='my-4 ml-11 inline-block ' to="/find-talent">
                 <Button leftSection={<IconArrowLeft size={20} />} color='bright-sun.4' variant='light'>Back</Button>
             </Link>
-             <div className='flex gap-5 ml-4'>
+             <div className='flex gap-5 justify-center'>
                 <Profile {...profile} />
                 <RecommendTalent />
              </div>
