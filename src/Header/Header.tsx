af05022800210af856bd40f306ebf7a0a451fa51
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2026-04-03 22:58:58 +0530

    update --> click logo rederet to home page

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,14 +2,17 @@ import { Avatar, Indicator } from "@mantine/core";
 import avatar from "../assets/avatar.png";
 import { IconAnchor, IconAsset, IconBell, IconSettings } from "@tabler/icons-react";
 import NavLisks from "./NavLisks";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return <div className="w-full text-white bg-mine-shaft-950 px-6 h-28 flex justify-between items-center  font-['poppins']">
 
+        <Link to="/">
         <div className="flex gap-1 items-center text-bright-sun-400">
             <IconAnchor className="h-8 w-8" stroke={2.5} />
             <div className="text-2xl font-semibold">FindMyJob</div>
         </div>
+        </Link>     
         {NavLisks()}
         <div className="flex gap-3 items-center">
 
